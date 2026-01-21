63941ac1e6b8d71bdc6511dc8225c59d4a6124fa
Jordan Patten <jordypatten@Jordans-MacBook-Air-4.local>
2026-01-20 21:01:19 -0700

    Resistor and Light Circuit Setup

--- Arduino stuff/src/main.cpp
+++ Arduino stuff/src/main.cpp
@@ -2,39 +2,29 @@
 #include <Arduino_MKRIoTCarrier.h>
 
 MKRIoTCarrier carrier;
+int ledPin = A1; // Use Pin 1 for a breadboard LED
+int buttonPin = A3; // Use Pin 3 for a breadboard button
 
 void setup() {
-  // The carrier board is in the plastic case right now so we set this to true
+  // The carrier board is not in the plastic case right now so we set this to false
   // This helps the library adjust brightness and other settings, especially with the capacitive touch sensors, accordingly
-  CARRIER_CASE = true;
+  CARRIER_CASE = false;
   
   // Initializing the carrier board, so it knows that we are using it
-  carrier.begin();
+  //carrier.begin();
+
+  pinMode(ledPin, OUTPUT);
+  // INPUT_PULLUP makes the pin "HIGH" by default. 
+  //Basically, it adds the pullup resistor so that we don't need to add an external one, which is pretty lit
+ // When you press the button to GND, it becomes "LOW".
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  // Red light, flash for half a second
-  carrier.leds.fill(carrier.leds.Color(255, 0, 0), 0, 5);
-  carrier.leds.show();
-  delay(500);
-
-  // Green light, flash for half a second
-  carrier.leds.fill(carrier.leds.Color(0, 255, 0), 0, 5);
-  carrier.leds.show();
-  delay(500);
-
-  // Blue light, flash for half a second
-  carrier.leds.fill(carrier.leds.Color(0, 0, 255), 0, 5);
-  carrier.leds.show();
-  delay(500);
-
-  // White light (all colors mixed), flash for half a second
-  carrier.leds.fill(carrier.leds.Color(255, 255, 255), 0, 5);
-  carrier.leds.show();
-  delay(500);
-  
-  // OFF
-  carrier.leds.clear();
-  carrier.leds.show();
-  delay(500);
+  // Check if button is pressed (connected to GND)
+  if (digitalRead(buttonPin) == LOW) {
+    digitalWrite(ledPin, HIGH); // LED ON
+  } else {
+    digitalWrite(ledPin, LOW);  // LED OFF
+  }
 }
